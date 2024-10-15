23970e240de1bc3733932be9844be780494f5e95
LiviaFanini <167907827+LiviaFanini@users.noreply.github.com>
2024-10-15 15:02:42 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Programação',
+    'O que é uma função anônima?',
+    'É uma forma de simplificar a criação de funções menores e de executar o código exatamente no local que foi escrito.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o nome da capítal da Austrália?',
+    'A capital da Austrália é Camberra'
+)
+
+criaCartao(
+    'Química',
+    'O que é um íon?',
+    'É uma espécie atômica eletricamente carregada'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como podemos usar o verbo to be?',
+    'Ele é usado para indicar estados, identificar pessoas ou coisas e formar tempos verbais.'
+)
+
+criaCartao(
+    'Matemática',
+    'qual fórmulas usamos para calcular equações de segundo grau?',
+    'A fórmula de Bhaskara'
+)
+
+criaCartao(
+    'Língua Potuguesa',
+    'Que tipo de gênero textual é o conto?',
+    'O gênero textual do conto é narrativo'
+)
